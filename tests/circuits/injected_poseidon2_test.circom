9ebf32a92f34469fda36539f74d9d447232c3730
0xThemis <118167989+0xThemis@users.noreply.github.com>
2026-08-27 12:46:58 +0200

    refactor: remove injection template

--- tests/circuits/injected_poseidon2_test.circom
+++
@@ -1,5 +0,0 @@
-pragma circom 2.2.2;
-
-include "mpc.circom";
-
-component main = TACEO_INJECTED_Poseidon2(3);
--- tests/mpc.test.js
+++ tests/mpc.test.js
@@ -1,21 +1,9 @@
 const { wasm } = require("circom_tester");
 const path = require("path");
-const { INCLUDE, POSEIDON2_T3_KAT0_OUT } = require("./common");
+const { INCLUDE } = require("./common");
 
 describe("MPC intrinsics wiring", function () {
   this.timeout(60000);
-
-  describe("TACEO_INJECTED_Poseidon2", function () {
-    it("wires to Poseidon2 (t=3 kat0)", async () => {
-      const circuit = await wasm(
-        path.join(__dirname, "circuits/injected_poseidon2_test.circom"),
-        { include: INCLUDE },
-      );
-      const witness = await circuit.calculateWitness({ in: [0, 1, 2] }, true);
-      await circuit.assertOut(witness, { out: POSEIDON2_T3_KAT0_OUT });
-      await circuit.checkConstraints(witness);
-    });
-  });
 
   describe("TACEO_REVEAL", function () {
     it("preserves its input values", async () => {
